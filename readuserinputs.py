817cae33f46457a58e7f9c0ab7604a5643244cd2
Vishal <58520580+vxsharma-14@users.noreply.github.com>
2020-11-26 22:36:50 -0600

    Add files via upload
    - Changed function InputParam - removed CFL number reading, corrected sys.exit()
    - InputParam() function moved from preprocess.py to readuserinputs.py
    - Added function ReadCoefficients to read a text file containing PDE coefficients and CFL number

---
+++ readuserinputs.py
@@ -0,0 +1,124 @@
+# coding: utf-8
+import sys
+
+#*******************************************************************************
+def InputParam(InFileName='./Input/SimulationSetUp.dat'):
+    '''Read input parameters for the simulation set-up from a saved file
+
+    Call signature;
+
+        InputParam(InFileName)
+
+    Parameters
+    __________
+
+    InFileName: str, default: './Input/SimulationSetUp.dat'
+                The string value representing the file to be read for
+                simulation inputs. Use ".dat" or ".txt" file extensions
+    '''
+
+    import sys
+    
+    print(f'Reading Input Parameters from file {InFileName}.')
+    InFile = open(InFileName, 'r')
+    #InFile.seek(0)
+    
+    Line1 = (InFile.readline()).split()
+    if Line1[0] != "EXPERIME":
+        print(f'{Line1[0]}: Record Missing or Misplaced in Line 1')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+        
+    Line2 = (InFile.readline()).split()
+    if Line2[0] != "GRIDPOINT":
+        print(f'{Line2[0]}: Record Missing or Misplaced in Line 2')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+    
+    Line3 = (InFile.readline()).split()
+    if Line3[0] != "DIMENSION":
+        print(f'{Line3[0]}: Record Missing or Misplaced in Line 3')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+        
+    Line4 = (InFile.readline()).split()
+    if Line4[0] != "CONVERG":
+        print(f'{Line4[0]}: Record Missing or Misplaced in Line 5')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+    
+    Line5 = (InFile.readline()).split()
+    if Line5[0] != "FILENAM":
+        print(f'{Line5[0]}: Record Missing or Misplaced in Line 6')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+    
+    Line6 = (InFile.readline()).split()
+    if Line6[0] != "FRAMESCAP":
+        print(f'{Line6[0]}: Record Missing or Misplaced in Line 7')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+    
+    ExpNumber = Line1[1]
+    iMax, jMax = [int(Line2[1]), int(Line2[2])]
+    L, H = [float(Line3[1]), float(Line3[2])]
+    ConvCriteria, nMax = [float(Line4[1]), int(Line4[2])]
+    nWrite, OutFileName, nDisplay, HistFileName = [int(Line5[1]), Line5[2]\
+                                                  ,int(Line5[3]), Line5[4]]
+    FrameOpt, FrameWrite = [int(Line6[1]), (Line6[2])]
+    print('Simulation set up done.')
+
+    OutFileName = './Output/' + OutFileName
+    HistFileName = './Output/' + HistFileName
+    
+    # Return the data using dataclass method
+    return (ExpNumber, iMax, jMax, L, H, ConvCriteria, nMax,\
+            nWrite, OutFileName, nDisplay, HistFileName, FrameOpt,\
+            FrameWrite)
+
+#*********************************************************************************
+def ReadCoefficients(InFileName='./Input/Coefficients.dat'):
+    '''Read the coefficients - diffusivity that occur in the parabolic
+       PDEs and the CFL number provided as user input
+       
+    Call signature;
+
+        ReadCoefficients(InFileName)
+
+    Parameters
+    __________
+
+    InFileName: str, default: './Input/Coefficients.dat'
+                The string value representing the file to be read for
+                simulation inputs. Use ".dat" or ".txt" file extensions
+    '''
+    
+    
+    print(f'Reading coefficients file from file {InFileName}.')
+    InFile = open(InFileName, 'r')
+    
+    Line1 = InFile.readline()
+        
+    Line2 = (InFile.readline()).split()
+    if Line2[0] != "COURANT":
+        print(f'{Line2[0]}: Record Missing or Misplaced in Line 2')
+        print(f'Check File {InFileName}')
+        sys.exit('STOP')
+    
+    Line3 = (InFile.readline()).split()
+    if Line3[0] != "DIFFUSIV":
+        print(f'{Line3[0]}: Record Missing or Misplaced in Line 3')
+        print(f'Check File {InFileName}')
+        sys.exit(STOP)
+        
+    print('Coefficient values assigned.')
+    
+    CFL = float(Line2[1])
+    Diff = float(Line3[1])
+    
+    # Return the data using dataclass method
+    return (CFL, Diff)
+
+#*******************************************************************************
+#if __name__ == '__main__':
+#    InputParam()
