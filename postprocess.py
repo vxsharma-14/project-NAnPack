b5aa36e88fa8f6b06849e305781dd0fe67eea30c
Vishal <58520580+vxsharma-14@users.noreply.github.com>
2020-12-07 22:47:36 -0600

    Add files via upload

--- postprocess.py
+++ postprocess.py
@@ -3,7 +3,31 @@ import numpy as np
 
 #***********************************************************************************
 def WriteSolutionToFile(OutFileName, iMax, jMax, dX, dY, U):
-    '''Function to write simulation output to files'''
+    '''Write simulation results of the entire domain to output file.
+
+    Call Signature:
+
+        WriteSolutionToFile(OutFileName, iMax, jMax, dX, dY, U)
+
+    Parameters
+    __________
+
+    OutFileName: string
+
+                 File path and name of the output file.
+
+    iMax, jMax: int, int
+
+                Grid points in x and y directions, respectively.
+
+    dX, dY: float, float
+
+            Grid step sizes in x and  y direction, respectively.
+
+    U: numpy 2D array, float
+
+       Solution of the dependent variable to be stored.
+    '''
     
     OutFile = open(OutFileName, "w")
     
@@ -15,7 +39,35 @@ def WriteSolutionToFile(OutFileName, iMax, jMax, dX, dY, U):
 
 #***********************************************************************************
 def WriteConvHistToFile(HistFileName, n, Error, WriteFlag='NO', PrintMsg='NONE'):
-    '''Function to write convergence history log'''
+    '''Write convergence history log
+
+    Call Signature:
+
+        WriteConvHistToFile(HistFileName, n, Error, WriteFlag, PrintMsg)
+
+    Parameters
+    __________
+
+    HistFileName: string
+
+                  File path and name of the output file.
+
+    n: int
+
+       Iteration level (time level).
+
+    Error: float
+
+           Error in the solution at each iteration level.
+
+    WriteFlag: boolean (YES/NO), default: 'NO'
+
+               Internal flag to control print message at the end.
+
+    PrintMsg: string, default: 'NONE'
+
+              Internal message stored at the end of the convergence file.
+    '''
     
     if WriteFlag == 'NO':
         if n == 1:
@@ -36,3 +88,59 @@ def WriteConvHistToFile(HistFileName, n, Error, WriteFlag='NO', PrintMsg='NONE')
     HistFile.close()
 
 #***********************************************************************************
+def Write1DSolutionToFile(Out1DFileName, U, Direction):
+    '''Write output data in 1D format at nodes of X or Y
+
+    Call Signature:
+
+        WriteSolutionToFile(OutFileName, iMax, jMax, dX, dY, U)
+
+    Parameters
+    __________
+
+    Out1DFileName: string
+
+                   File path and name of the 1D output file.
+
+    U: numpy 2D array, float
+
+       Solution of the dependent variable to be stored.
+
+    Direction: string, either 'X' or 'Y'
+
+               Define directions in which 1D results will be stored.
+               If Direction = 'X', results will be stored along X-axis at
+                                   various Y-nodes (max 5 nodes).
+               If Direction = 'Y', results will be stored along Y-axis at
+                                   various X-nodes (max 5 nodes).
+    '''
+
+    from readuserinputs import iMax, jMax, Length, Height
+
+    dX = Length/(iMax - 1)
+    dY = Height/(jMax - 1)
+    
+    OutFile = open(Out1DFileName, "w")
+    
+    if Direction == 'Y':
+        iX = [11, 15, 20, 25, 26]
+        X = [dX*i for i in iX]
+        print(f'{"Y":^10} {"X=":>6}{X[0]:<6} {"X=":>6}{X[1]:<6} {"X=":>6}{X[2]:<6}\
+{"X=":>6}{X[3]:<6} {"X=":>6}{X[4]:<6}',file=OutFile)
+        for j in range(0,jMax):
+            print(f'{dY*j:<10.6f} {U[iX[0]][j]:>12.8f} {U[iX[1]][j]:>12.8f}\
+{U[iX[2]][j]:>12.8f} {U[iX[3]][j]:>12.8f} {U[iX[4]][j]:>12.8f}', file=OutFile)
+            
+    elif Direction == 'X':
+        jY = [11, 15, 20, 25, 26]
+        Y = [dY*j for j in jY]
+        print(f'{"X":^10} {"Y=":>6}{Y[0]:<6} {"Y=":>6}{Y[1]:<6} {"Y=":>6}{Y[2]:<6}\
+{"Y=":>6}{Y[3]:<6} {"Y=":>6}{Y[4]:<6}',file=OutFile)
+        for i in range(0,iMax):
+            print(f'{dX*i:<10.6f} {U[i][jY[0]]:>12.8f} {U[i][jY[1]]:>12.8f}\
+{U[i][jY[2]]:>12.8f} {U[i][jY[3]]:>12.8f} {U[i][jY[4]]:>12.8f}', file=OutFile)
+            
+    else:
+        print("Check Direction input again, use either 'X' or 'Y' (case-sensitive)")
+
+    OutFile.close()
--- preprocess.py
+++ preprocess.py
@@ -23,16 +23,16 @@ def BC(U, iMax, jMax, BCType):
 
     if BCType == 'Dirichlet': # Dirichlet boundary conditions
         # along j = 1
-        U[0:-1, 0] = U_100
+        U[0:-1, 0] = 40.0
 
         # along j = jMax
-        U[0:-1, -1] = U_0
+        U[0:-1, -1] = 10.0
 
         # along i = 1
-        U[0, 0:-1] = U_0
+        U[0, 0:-1] = 0.0
 
         # along i = iMax
-        U[-1, 0:j1] = U_0
+        U[-1, 0:j1] = 0.0
 
     elif BCType == 'Neumann': # Neumann boundary conditions
 
@@ -83,86 +83,6 @@ def Initial(iMax, jMax):
     return U
 
 #*********************************************************************************
-def InputParam(InFileName='./Input/InputParameters.dat'):
-    '''Read input parameters for the simulation set-up from a saved file
-
-    Call signature;
-
-        InputParam(InFileName)
-
-    Parameters
-    __________
-
-    InFileName: str, default: 'InputParameters.dat'
-                The string value representing the file to be read for
-                simulation inputs. Use ".dat" or ".txt" file extensions
-    '''
-
-    import sys
-    
-    print(f'Reading Input Parameters from file {InFileName}.')
-    InFile = open(InFileName, 'r')
-    #InFile.seek(0)
-    
-    Line1 = (InFile.readline()).split()
-    if Line1[0] != "EXPERIME":
-        print(f'{Line1[0]}: Record Missing or Misplaced in Line 1')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-        
-    Line2 = (InFile.readline()).split()
-    if Line2[0] != "GRIDPOINT":
-        print(f'{Line2[0]}: Record Missing or Misplaced in Line 2')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-    
-    Line3 = (InFile.readline()).split()
-    if Line3[0] != "DIMENSION":
-        print(f'{Line3[0]}: Record Missing or Misplaced in Line 3')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-        
-    Line4 = (InFile.readline()).split()
-    if Line4[0] != "COURANT":
-        print(f'{Line4[0]}: Record Missing or Misplaced in Line 4')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-        
-    Line5 = (InFile.readline()).split()
-    if Line5[0] != "CONVERG":
-        print(f'{Line5[0]}: Record Missing or Misplaced in Line 5')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-    
-    Line6 = (InFile.readline()).split()
-    if Line6[0] != "FILENAM":
-        print(f'{Line6[0]}: Record Missing or Misplaced in Line 6')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-    
-    Line7 = (InFile.readline()).split()
-    if Line7[0] != "FRAMESCAP":
-        print(f'{Line7[0]}: Record Missing or Misplaced in Line 7')
-        print(f'Check File {InFileName}')
-        sys.exit(0)
-    
-    ExpNumber = Line1[1]
-    iMax, jMax = [int(Line2[1]), int(Line2[2])]
-    L, H = [float(Line3[1]), float(Line3[2])]
-    CFL = float(Line4[1])
-    ConvCriteria, nMax = [float(Line5[1]), int(Line5[2])]
-    nWrite, OutFileName, nDisplay, HistFileName = [int(Line6[1]), Line6[2]\
-                                                  ,int(Line6[3]), Line6[4]]
-    FrameOpt, FrameWrite = [int(Line7[1]), (Line7[2])]
-    print('Simulation set up done')
-
-    OutFileName = './Output/' + OutFileName
-    HistFileName = './Output/' + HistFileName
-    
-    # Return the data using dataclass method
-    return (ExpNumber, iMax, jMax, L, H, ConvCriteria, nMax,\
-            nWrite, OutFileName, nDisplay, HistFileName, FrameOpt,\
-            FrameWrite)
 
 #*********************************************************************************
 if __name__ == '__main__':
